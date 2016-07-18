6c489217e5aff8448a9a1597c063563a051dcd99
chriswang1990 <1990chriswang1990@gmail.com>
2016-07-18 09:47:19 -0700

    Initial commit for the Retrofit example

---
+++ app/src/main/java/com/example/chriswang/retrofit/GithubUser.java
@@ -0,0 +1,11 @@
+package com.example.chriswang.retrofit;
+
+public class GithubUser {
+    String login;
+    String name;
+
+    @Override
+    public String toString() {
+        return login;
+    }
+}
---
+++ app/src/main/java/com/example/chriswang/retrofit/GithubUserAPI.java
@@ -0,0 +1,11 @@
+package com.example.chriswang.retrofit;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface GithubUserAPI {
+    String ENDPOINT = "https://api.github.com";
+    @GET("/users/{user}")
+    Call<GithubUser> getUser(@Path("user") String user);
+}
